0a6253bfeeda47dc457d9c604d429d2ffbe49736
Tim Menzies <timm@ieee.org>
2020-10-06 00:04:25 -0400

    save

--- src/lib/sample.lua
+++ src/lib/sample.lua
@@ -1,32 +1,22 @@
+local s={}
 
-local sample={}
+local r = math.random
 
-function sample.rnd(x) return math.floor((x+0.5)//1) end
-function sample.r()    return math.random() end
-function sample.u(x,y) return x+(y-x)*sample.r() end
-
-function sample.tri(a,mode,b)
+function s.tri(a,mode,b)
   local c   = (mode-a)/(b-a) -- convert mode to 0..1
-  local u,v = sample.r(), sample.r()
+  local u,v = r(), r()
   c= (1-c)*math.min(u,v) + c*math.max(u,v)
   return a+(b-a)*c -- inject back into range
 end
 
-function sample.weilbull(lo,hi,k,l)
-  local x = sample.u(lo,hi)
-  if x<0 then return 0 end
-  return (k/l)*(x/l)^(k-1)*math.exp(-1*(x/l)^k)
-end
-
-function sample.norm(mu,sd) 
-  local r = math.random
+function s.norm(mu,sd) 
   mu = mu or 0
   sd = sd or 1
-  return mu + sd * ((-2 * math.log(sample.r()))^0.5 
-                        * math.cos(6.2831853*sample.r()))
+  return mu + sd * ((-2 * math.log(r()))^0.5 
+                        * math.cos(2*math.pi*r()))
 end
 
-function sample.bars(t)
+function s.bars(t)
   local total, tmp = 0, {}
   for key,one in pairs(t) do
     tmp[ #tmp+1 ] = {key=key, weight=one}
@@ -40,28 +30,60 @@ function sample.bars(t)
       if r<=0 then return x.key end end end
 end
 
-function sample.interpolate(t)
+function s.interpolate(t)
   local bars= {}
   for i=2,#t do
     local x1= t[i-1][1]
     local x2= t[i  ][1]
     bars[{x1,x2}] = (x2-x1)*t[i-1][2]
   end
-  local f=sample.bars(bars)
+  local f=s.bars(bars)
   return function()
     local bar=f()
-    return sample.u(bar[1],bar[2])
+    local x1,x2 = bar[1],bar[2]
+    return x1 + (x2-x1)*math.random()
   end
 end
 
---z=f0(y1,y2...)
---y1=f1(x1),
---y2=f2(x2)
+function s.g(xy)
+  xy.y={}
+  for k,x in pairs(xy.x) do xy.x[x]=xy.meta.x[k].cap(x)     end
+  for k,y in pairs(xy.y) do xy.y[k]=xy.meta.y[k].eval(xy.x) end
+  return xy
+end
 
-m={g=function(i) return i.a + i.b / i.c end, 
-   x={a={lo=10,hi=20,  weilbull={5,1}},
-      b={lo=10,hi=20,  norm={15,2}},
-      c={lo=0,hi=20,   sample={{0,0},{1,10},{2,10},{20,0}}}}}
+function s.f(model)
+  local xy =  {meta=meta,x={},y={}}
+  for k,x in pairs(model.x) do xy.x[k] = x.eval() end
+  for k,y in pairs(model.y) do xy.y[k] = y.eval(out.x) end
+  return xy
+end
 
+function s.cap(x,lo,hi)  return x<lo and lo or x>hi and hi or x end
+function s.wrap(x,lo,hi) return lo + x % (hi-lo) end
 
-return sample
+function s.uniformed(lo,hi,cap)
+  return {f= "uniform", 
+          cap = cap and s.cap or s.wrap,
+          eval= function() return lo+(hi-lo)*math.random() end,
+          lo= lo, hi= hi} end
+
+function s.normaled(mu,sd,cap)
+  return {f="normal", 
+          cap = cap and s.cap or s.wrap,
+          eval=function() return s.norm(mu,sd) end,
+          lo=mu-3*sd, hi=mu+3*sd,mu=mu,sd=sd} end
+
+function s.tried(lo,mode,hi,cap)
+  return {f="triangular", 
+          cap = cap and s.cap or s.wrap,
+          eval=function() return s.tri(lo,mode,hi)  end,
+          lo=lo, hi=hi,mode=mode} end
+
+function s.interpolated(bars,cap)
+  return {f="normal", 
+          cap = cap and s.cap or s.wrap,
+          eval= s.interpolate(bars) ,
+          lo=bars[1][1], hi=bars[#bars][1] } end
+
+return s
--- src/tests/sample.lua
+++ src/tests/sample.lua
@@ -2,40 +2,39 @@ local lib=dofiled("lib/misc")
 local eg=dofiled("lib/eg").all
 local s=dofiled("lib/sample")
 
-eg {
-bar1 = function(    f,a)
-    a,f = {}, s.bars {a=1,b=2,c=4}
-    for i=1,20 do a[#a+1]= f() end
-    table.sort(a)
-    print(table.concat(a))
-end,
+local function rnd(x) return (x+0.5)//1 end
 
-tri1=function(   a)
-  a={}
-  for i=1,256 do a[#a+1] = s.rnd(s.tri(1,3,10)) end
+function bar1(    f,a)
+  a,f = {}, s.bars {a=1,b=2,c=4}
+  for i=1,20 do a[#a+1]= f() end
   table.sort(a)
   print(table.concat(a))
-end,
+end
 
-weil=function(   a)
+function tri1(   a)
   a={}
-  for i=1,256 do 
-    a[#a+1] = lib.round(s.weilbull(1,5,1,1),2)
-  end
+  for i=1,256 do a[#a+1] = rnd(s.tri(1,3,10)) end
   table.sort(a)
-  print(table.concat(a," "))
-end,
+  print(table.concat(a))
+end
 
-sample1=function(   f,a)
+function sample1(   f,a)
   a,f={},s.interpolate{{1,5},{2,20},{3,5},{4,5},{8,5}}
   for i=1,2048 do 
-     local x = s.rnd(f())
+     local x = rnd(f())
      a[x] = (a[x] or 0) + 1 
   end
   for i,j in pairs(a) do print(i,j) end
 end
-}
 
+--eg {bar1=bar1, tri1=tri1, sample1=sample1}
 
+m={y={f1={eval=function(z) return z.a/(z.b+z.c) * z.d end,
+          more=true}},
+   x={a=s.uniformed(1,20),
+      b=s.normaled(15,2),
+      c=s.tried(10,20,50),
+      d=s.interpolated({{0,0},{1,10},{2,10},{20,0}})}}
 
+--for i=lib.oo(s.f(m))
 
